d1c1e811df49f91432b5207b92d7e133ee8d6fbf
Honore Jabiro <honorejabiro@gmail.com>
2025-06-30 18:14:00 -0500

    Implemented REVISE function

--- solver.py
+++ solver.py
@@ -53,7 +53,45 @@ def order_domain_values(var, assignment, csp):
     sorted_vars = sorted(hashmap, key=lambda val: hashmap[val])
     return sorted_vars
 
+"""
+Implementing the Revised function for the AC3 function
+"""
+def REVISE(csp, neighbor):
+    result = False
+    X, Y = neighbor
 
+    for x in csp.domains[X].copy():
+        for y in csp.domains[Y]:
+            if x == y:
+                csp.domains[X].remove(x)
+                result = True
+    
+    return result
+
+
+"""
+Implementing the ac3 algorithm
+"""
+def ac3(csp, queue):
+    inferences = {}
+    while queue:
+        pair = queue.pop()
+        if REVISE(csp, pair):
+            if not csp.domains[pair[0]]:
+                return False
+            inferences[pair[0]] = csp.domains[pair[0]].copy()
+            for Z in csp.neighbors[pair[0]] - {pair[1]}:
+                queue.append((Z, pair[0]))
+    return inferences
+    
+
+"""
+Implement all inferences with each assignment given
+"""
+def inference(csp, assignment, var):
+    queue = deque()
+    queue = [(neighbor, var) for neighbor in csp.neighbors[var]]
+    return ac3(csp, queue)
         
 """
 Backtracking function that assigns numbers to empty cells and backtracks when the number is invalid
@@ -70,9 +108,10 @@ def Backtrack(assignment, csp) -> dict:
         if consistent(value, assignment, var, csp):
             assignment[var] = value
             saved_domains = deepcopy(csp.domains)
+            csp.domains[var] = {value}
 
-            # Maintaing Arc Consistency, this is where the inference occurs we check if the assignment 
-            if MAC(csp, var)
+            if inference(csp, assignment, var, ):
+
 
     
 def main(sys):
